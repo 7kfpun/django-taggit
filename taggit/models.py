4e9f6993c939ac7843d29e03ddc3c448b37009f9
kf <7kfpun@gmail.com>
2015-07-17 11:56:04 +0800

    change object_id type to CharField

--- taggit/models.py
+++ taggit/models.py
@@ -148,7 +148,7 @@ class TaggedItemBase(ItemBase):
 
 
 class GenericTaggedItemBase(ItemBase):
-    object_id = models.IntegerField(verbose_name=_('Object id'), db_index=True)
+    object_id = models.CharField(verbose_name=_('Object id'), db_index=True, max_length=100)
     content_type = models.ForeignKey(
         ContentType,
         verbose_name=_('Content type'),
